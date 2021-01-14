6ec93653ce9404c0ae5e6dc7bd9aa45f85724017
ReidBarbeln2022 <814684@mystma.org>
2021-01-14 16:06:42 +0000

    caculator try 1

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-#WRITE YOUR CODE IN THIS FILE
+def calculate(p, x, y):
+    if p == "add":
+        return x + y
+    elif p == "subtract":
+        return x - y
+    elif p == "multiply":
+        return x * y
+    elif p == "divide":
+        return x / y
+
+print(calculate("subtract", 5, 5))
